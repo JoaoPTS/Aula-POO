d8e8b98524ac06048bf9a53c2d609aa3c0ea9925
JoaoPTS <joao.souza123121@gmail.com>
2026-08-17 21:53:06 -0300

    Commit final de aula

---
+++ Aula 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using ClassProduto;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Produto produto = new Produto();
+        Console.WriteLine("Digite o nome do produto:");
+        produto.Nome = Console.ReadLine();
+        Console.WriteLine("Digite o preço do produto:");
+        produto.Preco = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite a quantidade do produto:");
+        produto.Quantidade = Convert.ToDouble(Console.ReadLine());
+
+        produto.ExibirInformacoes();
+        produto.ValorTotalEstoque();
+
+        Console.WriteLine("Adicionar quantidade ao estoque:");
+        produto.AdicionarProdutos(Convert.ToDouble(Console.ReadLine()));
+
+        produto.ExibirInformacoes();
+        produto.ValorTotalEstoque();
+
+        Console.WriteLine("Remover quantidade do estoque:");
+        produto.RemoverProdutos(Convert.ToDouble(Console.ReadLine()));
+
+        produto.ExibirInformacoes();
+        produto.ValorTotalEstoque();
+    }
+}
