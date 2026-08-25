80ef1c64207f990c2f652f83fd06c2cdb4593726
JoaoPTS <joao.souza123121@gmail.com>
2026-08-24 21:57:09 -0300

    Aula de encapsulamento

---
+++ Aula4/EncapsulamentoConta/Program.cs
@@ -0,0 +1,12 @@
+﻿using EncapuslamentoConta;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        conta c1 = new conta();
+        //c1.numero = 1; este atributo não pode ser acessado, pois está encapsulado
+        c1.Numero = 1; //Está chamando o método set, que está encapsulando o atributo numero
+        Console.WriteLine("Número da conta" + c1.Numero); //Está chamando o método get, que está encapsulando o atributo numero
+    }
+}
