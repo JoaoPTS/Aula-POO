c16cdc9a95d48cef2fde1b29fc89598f55884137
JoaoPTS <joao.souza123121@gmail.com>
2026-09-18 21:19:28 -0300

    commit rapido

--- Exercicios/ConstrutorAluno/ContrutorAluno.cs
+++ Exercicios/ConstrutorAluno/ContrutorAluno.cs
@@ -7,10 +7,15 @@ namespace ConstrutorAluno
 {
     public class Aluno
     {
-        public long RA { get; set; }
+        public long RA { get; set; } = 1570482423000;
         public string? Nome { get; set; }
-        public int contador { get; set; } = 0;
 
+        public static int Contador { get; set; } = 0;
         
+        public Aluno(string nome){
+            Contador++;
+            this.RA += Contador;
+            this.Nome = nome;
+        }
     }
 }
--- Exercicios/ConstrutorAluno/Program.cs
+++ Exercicios/ConstrutorAluno/Program.cs
@@ -4,6 +4,15 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        Aluno aluno1 = new Aluno("Aluno 1");
+        Aluno aluno2 = new Aluno("Aluno 2");
+        Aluno aluno3 = new Aluno("Aluno 3");
+
+        Console.WriteLine("Alunos Cadastrados:");
+        Console.WriteLine(aluno1.RA + " - " + aluno1.Nome);
+        Console.WriteLine(aluno2.RA + " - " + aluno2.Nome);
+        Console.WriteLine(aluno3.RA + " - " + aluno3.Nome);
+
+        Console.WriteLine("\nQuantidade de instâncias: " + Aluno.Contador);
     }
 }
